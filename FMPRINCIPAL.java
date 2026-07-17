1a7ffcd57a87a3b467dae81e1ec4b87c4aca01ec
jhomerson salcedo <jhomerson05@gmail.com>
2026-07-16 21:16:13 -0500

    primer avance

---
+++ FMPRINCIPAL.java
@@ -0,0 +1,96 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JButton;
+import javax.swing.JCheckBoxMenuItem;
+
+public class FMPRINCIPAL extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JMenuBar menuBar;
+	private JMenu mnNewMenu;
+	private JMenu mnNewMenu_1;
+	private JMenu mnNewMenu_2;
+	private JMenu mnNewMenu_3;
+	private JMenu mnNewMenu_4;
+	private JMenuItem mntmNewMenuItem;
+	private JMenuItem mntmNewMenuItem_1;
+	private JMenuItem mntmNewMenuItem_2;
+	private JMenuItem mntmNewMenuItem_3;
+	private JMenuItem mntmNewMenuItem_4;
+	private JMenuItem mntmNewMenuItem_5;
+	private JMenuItem mntmNewMenuItem_6;
+	private JMenuItem mntmNewMenuItem_7;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FMPRINCIPAL frame = new FMPRINCIPAL();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FMPRINCIPAL() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		
+		menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		mnNewMenu = new JMenu("Archivo");
+		menuBar.add(mnNewMenu);
+		
+		mntmNewMenuItem = new JMenuItem("Salir");
+		mnNewMenu.add(mntmNewMenuItem);
+		
+		mnNewMenu_1 = new JMenu("Mantinimiento");
+		menuBar.add(mnNewMenu_1);
+		
+		mntmNewMenuItem_1 = new JMenuItem("Consultar Celular");
+		mnNewMenu_1.add(mntmNewMenuItem_1);
+		
+		mntmNewMenuItem_2 = new JMenuItem("Modificar Celular");
+		mnNewMenu_1.add(mntmNewMenuItem_2);
+		
+		mntmNewMenuItem_3 = new JMenuItem("Listar Celular");
+		mnNewMenu_1.add(mntmNewMenuItem_3);
+		
+		mnNewMenu_2 = new JMenu("Ventas");
+		menuBar.add(mnNewMenu_2);
+		
+		mntmNewMenuItem_4 = new JMenuItem("Vender");
+		mnNewMenu_2.add(mntmNewMenuItem_4);
+		
+		mnNewMenu_3 = new JMenu("Configuracion");
+		menuBar.add(mnNewMenu_3);
+		
+		mntmNewMenuItem_5 = new JMenuItem("Configurar descuentos");
+		mnNewMenu_3.add(mntmNewMenuItem_5);
+		
+		mntmNewMenuItem_6 = new JMenuItem("Configurar obsequios");
+		mnNewMenu_3.add(mntmNewMenuItem_6);
+		
+		mnNewMenu_4 = new JMenu("Ayuda");
+		menuBar.add(mnNewMenu_4);
+		
+		mntmNewMenuItem_7 = new JMenuItem("Acerca de Tienda");
+		mnNewMenu_4.add(mntmNewMenuItem_7);
+
+	}
+}
